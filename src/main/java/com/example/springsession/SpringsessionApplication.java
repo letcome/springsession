64f7c80ca3ec5ee2909d4d67453d9674201bc543
letcome <letcome@gmx.com>
2018-05-24 23:25:40 +0800

    Initial commit

---
+++ src/main/java/com/example/springsession/SpringsessionApplication.java
@@ -0,0 +1,12 @@
+package com.example.springsession;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringsessionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringsessionApplication.class, args);
+	}
+}
